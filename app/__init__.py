9afaf8361f3c7abf7f981aee504eed321c41addc
Mickoy Banton <mickoybanton11@gmail.com>
2025-03-02 19:19:16 -0500

    the final code for lab 3

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,15 @@
 from flask import Flask
+from flask_mail import Mail
+from .config import Config
+from flask_wtf.csrf import CSRFProtect
+
+from flask_mail import Mail
+from .config import Config
 
 app = Flask(__name__)
+app.config.from_object(Config)
+mail = Mail(app)
+SECRET_KEY = 'Sup3r$3cretkey'
+csrf = CSRFProtect(app)
+            
 from app import views
